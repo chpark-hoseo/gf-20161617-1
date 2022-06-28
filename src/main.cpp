7875ba1b0d53b71e4c3e04d4acd8bf128e388ede
20161617 <70498169+20161617@users.noreply.github.com>
2022-06-28 14:17:16 +0900

    HelloSDL
    HelloSDL 3번

--- src/main.cpp
+++ src/main.cpp
@@ -2,9 +2,7 @@
 //
 // written by changhoonpark@gmail.com
 
-
-#include "main.h"
-
+/*
 
 SDL_Window*		g_pWindow = 0;
 SDL_Renderer*	g_pRenderer = 0;
@@ -205,3 +203,40 @@ void render()
 
 	SDL_RenderPresent(g_pRenderer);
 }
+*/
+
+
+#include "main.h"
+
+SDL_Window* g_pWindow = 0;
+SDL_Renderer* g_pRenderer = 0;
+
+int main(int argc, char* args[])
+{
+
+	if (SDL_Init(SDL_INIT_EVERYTHING) >= 0)
+	{
+
+		g_pWindow = SDL_CreateWindow("HelloSDL", SDL_WINDOWPOS_CENTERED,
+			SDL_WINDOWPOS_CENTERED, 640, 480, SDL_WINDOW_SHOWN);
+
+		if (g_pWindow != 0)
+		{
+			g_pRenderer = SDL_CreateRenderer(g_pWindow, -1, 0);
+		}
+	}
+	else
+	{
+		return 1;
+	}
+
+	SDL_SetRenderDrawColor(g_pRenderer, 0, 0, 0, 255);
+	SDL_RenderClear(g_pRenderer);
+	SDL_RenderPresent(g_pRenderer);
+
+	SDL_Delay(5000);
+	SDL_Quit();
+
+	return 0;
+}
+
